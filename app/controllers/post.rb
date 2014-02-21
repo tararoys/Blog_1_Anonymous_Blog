8dd81430162dc4d6a8c34959b522c05334f38d50
Tara Roys <tlroys@gmail.com>
2014-02-20 22:38:41 -0600

    Create and Edit post adds and deletes from database

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -3,8 +3,25 @@ get "/posts/:id/edit" do
   erb :edit
 end 
 
+post "/posts/:id/edit" do 
+  puts params[:post]
+  post = Post.find(params[:id])
+  puts post.inspect
+  post.update_attributes(params[:post])
+  puts post.inspect
+  redirect "/posts/#{params[:id]}"
+
+
+end
+
 get "/posts/new" do
   erb :new_post
+end
+
+post "/posts" do 
+  puts params
+  post = Post.create(params[:post])
+  redirect "/posts/#{post.id}"
 end
 
 get "/posts/:id" do 
