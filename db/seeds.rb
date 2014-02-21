69c02e986b0c546b5f965fc172162f355124fbb6
Tara Roys <tlroys@gmail.com>
2014-02-20 21:08:45 -0600

    Show index page with list of blog posts and tags
    fixed tags in seed file too.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ add   = Tag.create(text: "ADD"   )
 anger = Tag.create(text: "anger" )
 
 one_focus_add   = PostTag.create(post_id: one_focus.id, tag_id:   add.id)
-one_focus_anger = PostTag.create(post_id: two_focus.id, tag_id: anger.id)
+one_focus_anger = PostTag.create(post_id: one_focus.id, tag_id: anger.id)
 
-two_focus_add   = PostTag.create(post_id: one_focus.id, tag_id:   add.id)
+two_focus_add   = PostTag.create(post_id: two_focus.id, tag_id:   add.id)
 two_focus_anger = PostTag.create(post_id: two_focus.id, tag_id: anger.id)
 
